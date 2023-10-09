4d65b80a463b1848b5014b379464127bf65bfa11
cjrequena <carlosjose.requena@tui.com>
2023-10-09 11:04:12 +0200

    --

--- spring-cloud-stream-kinesis-sample/consumer/src/main/java/com/cjrequena/sample/common/Constants.java
+++ spring-cloud-stream-kinesis-sample/consumer/src/main/java/com/cjrequena/sample/common/Constants.java
@@ -10,5 +10,8 @@ package com.cjrequena.sample.common;
  */
 public class Constants {
   /** */
-  public static final String VND_SAMPLE_SERVICE_V1 = "vnd.sample-service.v1";
+  public static final String VND_SAMPLE_SERVICE_V1 = "application/vnd.sample-service.v1";
+  public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm";
+  public static final String DATE_FORMAT = "yyyy-MM-dd";
+
 }
--- spring-cloud-stream-kinesis-sample/consumer/src/main/java/com/cjrequena/sample/dto/FooDTO.java
+++ spring-cloud-stream-kinesis-sample/consumer/src/main/java/com/cjrequena/sample/dto/FooDTO.java
@@ -1,12 +1,33 @@
 package com.cjrequena.sample.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.UUID;
 
+import static io.swagger.v3.oas.annotations.media.Schema.AccessMode.READ_ONLY;
+
 @Data
+@ToString
+@EqualsAndHashCode(callSuper = false)
+@JsonPropertyOrder(value = {
+  "id",
+  "name"
+})
+@Schema
 public class FooDTO implements Serializable {
+
+  @JsonProperty(value = "id")
+  @Schema(accessMode = READ_ONLY)
   private UUID id;
+
+  @NotNull(message = "name is a required field")
+  @JsonProperty(value = "name", required = true)
   private String name;
 }
