380c1a7a235306f2b5f185edd4f15eb22360bd12
cjrequena <carlosjose.requena@tui.com>
2025-09-22 14:03:15 +0200

    --

--- spring-cloud-grpc-sample/account-service/src/main/java/com/cjrequena/sample/exception/service/AccountNotFoundException.java
+++ spring-cloud-grpc-sample/account-service/src/main/java/com/cjrequena/sample/exception/service/AccountNotFoundException.java
@@ -6,7 +6,7 @@ package com.cjrequena.sample.exception.service;
  * <p></p>
  * @author cjrequena
  */
-public class AccountNotFoundException extends RuntimeServiceException {
+public class AccountNotFoundException extends ServiceRuntimeException {
   public AccountNotFoundException(String message) {
     super(message);
   }
--- spring-cloud-grpc-sample/account-service/src/main/java/com/cjrequena/sample/exception/service/OptimisticConcurrencyException.java
+++ spring-cloud-grpc-sample/account-service/src/main/java/com/cjrequena/sample/exception/service/OptimisticConcurrencyException.java
@@ -6,7 +6,7 @@ package com.cjrequena.sample.exception.service;
  * <p></p>
  * @author cjrequena
  */
-public class OptimisticConcurrencyException extends RuntimeServiceException {
+public class OptimisticConcurrencyException extends ServiceRuntimeException {
   public OptimisticConcurrencyException(String message) {
     super(message);
   }
