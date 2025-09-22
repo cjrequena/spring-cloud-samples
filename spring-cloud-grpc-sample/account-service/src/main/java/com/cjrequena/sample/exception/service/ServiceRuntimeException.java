8fed51388b59838f1541e0c92d15c53c1faed5b9
cjrequena <carlosjose.requena@tui.com>
2025-09-22 14:01:57 +0200

    --

---
+++ spring-cloud-grpc-sample/account-service/src/main/java/com/cjrequena/sample/exception/service/ServiceRuntimeException.java
@@ -0,0 +1,25 @@
+package com.cjrequena.sample.exception.service;
+
+import lombok.ToString;
+
+/**
+ *
+ * <p></p>
+ * <p></p>
+ * @author cjrequena
+ */
+@ToString
+public abstract class ServiceRuntimeException extends RuntimeException {
+  public ServiceRuntimeException(Throwable ex) {
+    super(ex);
+  }
+
+  public ServiceRuntimeException(String message) {
+    super(message);
+  }
+
+  public ServiceRuntimeException(String message, Throwable ex) {
+    super(message, ex);
+  }
+
+}
