64b8e6dc4d73ef00cd99d2b37fb7aa7adde9582c
cjrequena <carlosjose.requena@tui.com>
2025-10-09 19:18:00 +0200

    --

--- spring-cloud-stream-kafka-sample/consumer/src/main/java/com/cjrequena/sample/service/ConsumerService2.java
+++ spring-cloud-stream-kafka-sample/consumer/src/main/java/com/cjrequena/sample/service/ConsumerService2.java
@@ -3,6 +3,7 @@ package com.cjrequena.sample.service;
 import com.cjrequena.sample.event.FooEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -15,6 +16,16 @@ public class ConsumerService2 implements Consumer<Flux<Message<FooEvent>>> {
 
   public Mono<Void> execute(Message<FooEvent> message) {
     log.info("New event notification: {}", message.getPayload());
+    FooEvent fooEvent = message.getPayload();
+    // Extract headers
+    MessageHeaders headers = message.getHeaders();
+    Object kafkaKey = headers.get("kafka_receivedMessageKey");
+    Object topic = headers.get("kafka_receivedTopic");
+    Object partition = headers.get("kafka_receivedPartitionId");
+    Object offset = headers.get("kafka_offset");
+    Object xTestHeader = headers.get("x-test-header");
+    log.info("Received from topic={}, partition={}, offset={}, key={}, payload={}", topic, partition, offset, kafkaKey, fooEvent);
+
     return Mono.empty();
   }
 
---
+++ spring-cloud-stream-kafka-sample/consumer/src/main/java/com/cjrequena/sample/service/ConsumerService3.java
@@ -0,0 +1,49 @@
+package com.cjrequena.sample.service;
+
+import com.cjrequena.sample.dto.FooDTO;
+import com.cjrequena.sample.event.FooEvent;
+import com.cjrequena.sample.mapper.FooMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+@Slf4j
+@Service("consumer3")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class ConsumerService3 implements Function<Flux<Message<String>>, Mono<Void>> {
+
+  private final FooMapper fooMapper;
+
+  private Mono<Void> handleMessage(Message<String> message) {
+    log.info("New event notification: {}", message);
+
+    String payload = message.getPayload();
+    // Extract headers
+    MessageHeaders headers = message.getHeaders();
+    Object kafkaKey = headers.get("kafka_receivedMessageKey");
+    Object topic = headers.get("kafka_receivedTopic");
+    Object partition = headers.get("kafka_receivedPartitionId");
+    Object offset = headers.get("kafka_offset");
+    Object headerTest = headers.get("x-test-header");
+
+    log.info("Received from topic={}, partition={}, offset={}, key={}, payload={}", topic, partition, offset, kafkaKey, payload);
+
+    FooEvent event = this.fooMapper.mapToFooEventFromJsonString(payload);
+    FooDTO fooDTO = event.getData();
+    return Mono.empty();
+  }
+
+  @Override
+  public Mono<Void> apply(Flux<Message<String>> messageFlux) {
+    return messageFlux
+      .concatMap(this::handleMessage) // sequential processing; use flatMap for parallel
+      .then();
+  }
+}
